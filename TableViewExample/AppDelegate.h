41bbeec68a6cba446e23e98530b38e760eeb91d1
hoomi <hoomi2010@gmail.com>
2014-03-06 09:06:01 +0000

    Initial Commit

---
+++ TableViewExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TableViewExample
+//
+//  Created by Hooman Ostovari on 06/03/2014.
+//  Copyright (c) 2014 Hooman Ostovari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TableViewExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TableViewExample
+//
+//  Created by Hooman Ostovari on 06/03/2014.
+//  Copyright (c) 2014 Hooman Ostovari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
